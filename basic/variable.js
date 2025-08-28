d30152c7b8d58659680596bfd1f5bd5708cb4aa0
Aman-sahu95 <asahu8843@gmail.com>
2025-08-28 20:53:31 +0000

    container

---
+++ basic/variable.js
@@ -0,0 +1,15 @@
+const accountId=1442
+let accountEmail="Aman@gmail.com"
+var accountPassword="134567"
+accountCity="delhi"
+
+//accountId=2// not allowed
+accountEmail="hhh@gmail.com"
+accountPassword="7786756"
+accountCity="noida"
+console.log(accountId);
+
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
