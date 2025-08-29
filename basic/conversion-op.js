94db81ab6df40cc9350cde2f7ae0a57d69139fe1
Aman-sahu95 <asahu8843@gmail.com>
2025-08-29 06:03:03 +0000

    conversion

---
+++ basic/conversion-op.js
@@ -0,0 +1,13 @@
+let score=22
+
+//console.log(typeof score);
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber)
+
+
+
+let isLoggedIn="aman"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof isLoggedIn);
+console.log(booleanIsLoggedIn);
