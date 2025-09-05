a937215d6e68938d2a60160638f2f2f24a39b622
Aman-sahu95 <asahu8843@gmail.com>
2025-09-05 12:42:13 -0900

    project-2

--- 7project/project-1/script.js
+++ 7project/project-1/script.js
@@ -6,20 +6,45 @@ buttons.forEach(function(button){
     button.addEventListener(`click`,function(e){
         console.log(e)
         console.log(e.target)
-        if(e.target.id===`grey`){
-              body.style.backgroundColor=e.target.id;
-        }
-        if(e.target.id===`red`){
-              body.style.backgroundColor=e.target.id;
-        }
-        if(e.target.id===`blue`){
-              body.style.backgroundColor=e.target.id;
-        }
-        if(e.target.id===`yellow`){
-              body.style.backgroundColor=e.target.id;
-        }
-        if(e.target.id===`purple`){
-              body.style.backgroundColor=e.target.id;
+
+// ----------------------using if case-----------------------------//
+
+        // if(e.target.id===`grey`){
+        //       body.style.backgroundColor=e.target.id;
+        // }
+        // if(e.target.id===`red`){
+        //       body.style.backgroundColor=e.target.id;
+        // }
+        // if(e.target.id===`blue`){
+        //       body.style.backgroundColor=e.target.id;
+        // }
+        // if(e.target.id===`yellow`){
+        //       body.style.backgroundColor=e.target.id;
+        // }
+        // if(e.target.id===`purple n`){
+        //       body.style.backgroundColor=e.target.id;
+        // }
+
+// --------------------using switch case-----------------//
+
+        switch (e.target) {
+            case grey:
+                body.style.backgroundColor=e.target.id;
+                break;
+                case red:
+                body.style.backgroundColor=e.target.id;
+                break;
+                case blue:
+                body.style.backgroundColor=e.target.id;
+                break;
+                case yellow:
+                body.style.backgroundColor=e.target.id;
+                break;
+                case purple:
+                body.style.backgroundColor=e.target.id;
+                break;
+            default:
+                break;
         }
     })
 });
---
+++ 7project/project-2/script.js
@@ -0,0 +1,19 @@
+const form=document.querySelector(`form`);
+
+// ---------this use case will give you empty----------//
+
+//const height=parseInt(document.querySelector(`#height`).value)
+
+form.addEventListener(`submit`,function(e){
+    e.preventDefault();
+
+    const height=parseInt(document.querySelector(`#height`).value);
+    const weight=parseInt(document.querySelector(`#weight`).value);
+    const results=document.querySelector(`#results`);
+
+    if( height < 0 || isNaN(height)) {
+        results.innerHTML=`Please give a valid height ${height}`;
+    }else{
+        results.innerHTML=`hello ${height}`;
+}
+});
